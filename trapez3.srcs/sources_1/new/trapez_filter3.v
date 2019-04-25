7f8a9b26c91a53f688c43e9de4afcd8ba462239f
Gulyas Robert <gulyasr01@gmail.com>
2019-04-25 10:30:41 +0200

    Initialize the project

---
+++ trapez3.srcs/sources_1/new/trapez_filter3.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 25.04.2019 10:16:30
+// Design Name: 
+// Module Name: trapez_filter3
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module trapez_filter3(
+
+    );
+endmodule
